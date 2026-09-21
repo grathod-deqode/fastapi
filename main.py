39bc957d336f277566c71222a0fa9b8bcde43f85
Gaurav <grathod@deqode.com>
2026-09-21 17:50:24 +0530

    create new API /items/{item_id}

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get('/')
 def home() :
     return {"message" : "Hello FastAPI"}
+
+
+@app.get("/items/{item_id}")
+def get_items(item_id : int):
+    return {"item_id" : item_id}
